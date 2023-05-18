a79f43da75489396f89739109b3d42c3817b67f6
evans <kumarshiva0707@gmail.com>
2023-05-18 23:33:02 +0530

    setup basic body-parser and middlewares

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const { PORT } = require('./config/serverConfig')
+
 const setupAndStartServer = async () =>{
 
     // create the express Object
     const app = express();
+
+    //middlewares
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}))
+
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
         // console.log(process.env);
