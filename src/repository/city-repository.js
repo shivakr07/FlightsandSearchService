d92fa49ad4eddb27a73bf6d0a15e6fc34806d3d0
evans <kumarshiva0707@gmail.com>
2023-06-04 22:25:20 +0530

    added minor fixes

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -37,8 +37,7 @@ class CityRepository {
             const city = await City.update(data, {
                 where : {
                     id : cityId
-                },
-                returning : true
+                }
             });
             // const city = await City.findByPk(cityId)
             // city.name = data.name;
